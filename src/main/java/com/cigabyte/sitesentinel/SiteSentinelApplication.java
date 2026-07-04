fe563e20ac246a3f4953ab8af45468fbf6570717
sigabyte <ibrahimciga@hotmail.com>
2026-07-04 12:10:43 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/cigabyte/sitesentinel/SiteSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.cigabyte.sitesentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteSentinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cigabyte/sitesentinel/SiteSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cigabyte.sitesentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
